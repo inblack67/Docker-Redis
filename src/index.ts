8728386d8c20dee1aa38a595e09bc232a9bf7cf9
Aman Bhardwaj <inblack>
2021-02-23 11:21:24 +0530

    redis authentication

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import express from 'express';
 import Redis from 'ioredis';
+import 'dotenv-safe/config';
 import 'colors';
 
 const main = async () => {
     const app = express();
-    const redisClient = new Redis();
 
+    const redisClient = new Redis(Number(process.env.REDIS_PORT), process.env.REDIS_HOST, {
+        password: process.env.REDIS_PASSWORD
+    });
 
     await redisClient.set('hello', 'worlds');
     const res = await redisClient.get('hello');
     console.log(res);
 
     console.log(`Redis is here`.blue.bold);
-    
+
     const PORT = 5000;
     app.listen(PORT, () => {
         console.log(`Server started on port ${ PORT }`.green.bold);
